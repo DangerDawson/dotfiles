14bc332ffee3453f7ead9d869685ee40f004e3a7
David Dawson <david.dawson@gmail.com>
2015-03-16 09:18:54 +0000

    add local files

---
+++ make_symlinks.sh
@@ -0,0 +1,7 @@
+mypath=`pwd`
+ln -s $mypath/aliases.local $HOME/.aliases.local
+ln -s $mypath/gitconfig.local $HOME/.gitconfig.local
+ln -s $mypath/vimrc.local $HOME/.vimrc.local
+ln -s $mypath/vimrc.bundles.local $HOME/.vimrc.bundles.local
+ln -s $mypath/zshrc.local $HOME/.zshrc.local
+ln -s $mypath/tmux.conf.local $HOME/.tmux.conf.local
